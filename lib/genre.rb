7e5e9bc41c16a18cacd6357de27d13c8ffa560f1
proactive-hashmap-4080 <kimpham54@gmail.com>
2019-04-28 20:07:45 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,11 +1,11 @@
 class Genre
-  attr_accessor :name, :songs
+  attr_accessor :name
   @@all = []
 
   def initialize(name)
     @name = name
     @@all.push(self)
-    @songs = []
+    # @songs = []
   end
 
   def self.all
